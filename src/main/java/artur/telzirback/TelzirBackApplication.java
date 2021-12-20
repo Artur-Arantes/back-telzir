f6de21a054dfff5ef61ac38516b7bde8c72b8f8f
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-20 09:31:11 -0300

    commit inicial

---
+++ src/main/java/artur/telzirback/TelzirBackApplication.java
@@ -0,0 +1,13 @@
+package artur.telzirback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelzirBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelzirBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/artur/telzirback/TelzirBackApplicationTests.java
@@ -0,0 +1,13 @@
+package artur.telzirback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelzirBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
